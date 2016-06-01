8c0db774f697daab7176e41e9f817e5b796a59d2
Joel Cano <joel@eucledian.com>
2016-06-01 00:06:58 -0500

    name fix

--- app/src/main/java/com/eucledian/comapp/root/RootActivity.java
+++ app/src/main/java/com/eucledian/comapp/root/RootActivity.java
@@ -70,8 +70,9 @@ public class RootActivity extends AppCompatActivity implements NavigationView.On
         navHeaderName = (TextView) header.findViewById(R.id.navHeaderName);
         navHeaderMail = (TextView) header.findViewById(R.id.navHeaderMail);
         AppUser user = app.getAppUser();
+        /**
         navHeaderName.setText(user.getFullName());
-        navHeaderMail.setText(user.getMail());
+        navHeaderMail.setText(user.getMail());*/
         // Set navigation listener
         navigationView.setNavigationItemSelectedListener(this);
         // Call first item in menu
