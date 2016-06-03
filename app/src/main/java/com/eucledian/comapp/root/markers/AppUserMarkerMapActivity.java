3bc2ea15080219b7c194d2f017af988258db6e61
Joel Cano <canito0890@gmail.com>
2016-06-03 18:31:22 -0500

    no internet map view

--- app/src/main/java/com/eucledian/comapp/root/markers/AppUserMarkerMapActivity.java
+++ app/src/main/java/com/eucledian/comapp/root/markers/AppUserMarkerMapActivity.java
@@ -12,6 +12,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.eucledian.comapp.App;
 import com.eucledian.comapp.Config;
@@ -35,6 +38,7 @@ import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.FragmentById;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_app_user_marker_map)
 public class AppUserMarkerMapActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
@@ -44,6 +48,7 @@ public class AppUserMarkerMapActivity extends AppCompatActivity implements Googl
     private Marker appMarker;
     private com.google.android.gms.maps.model.Marker marker;
     private Location location = null;
+    private boolean isConnected = true;
 
     private final int LOCATION_PERMS = 100;
 
@@ -59,19 +64,32 @@ public class AppUserMarkerMapActivity extends AppCompatActivity implements Googl
     @FragmentById
     protected MapFragment mapFragment;
 
+    @ViewById
+    protected LinearLayout noMapView;
+
+    @ViewById
+    protected TextView latitudeText;
+
+    @ViewById
+    protected TextView longitudeText;
+
+    @ViewById
+    protected TextView accuracyText;
+
 
     @AfterViews
     protected void init() {
         appMarker = dao.fromArgs(getIntent().getExtras());
         initClient();
-        ConnectivityManager cm =
-                (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        boolean isConnected = activeNetwork != null &&
-                activeNetwork.isConnectedOrConnecting();
+        isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
         if(isConnected){
             mapFragment.getMapAsync(this);
+        }else{
+            client.connect();
+            getFragmentManager().beginTransaction().hide(mapFragment).commit();
+            noMapView.setVisibility(View.VISIBLE);
         }
     }
 
@@ -101,7 +119,6 @@ public class AppUserMarkerMapActivity extends AppCompatActivity implements Googl
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if(requestCode == LOCATION_PERMS && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
             @SuppressWarnings("MissingPermission") Location location = LocationServices.FusedLocationApi.getLastLocation(client);
-            Log.d(Config.LOG_TAG, "Pig!!!" + location.toString());
             if (location != null) setPlace(location);
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -124,11 +141,17 @@ public class AppUserMarkerMapActivity extends AppCompatActivity implements Googl
 
     private void setPlace(Location location) {
         this.location = location;
-        LatLng place = new LatLng(location.getLatitude(), location.getLongitude());
-        marker = map.addMarker(new MarkerOptions()
-                .draggable(true)
-                .position(place));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 14));
+        if(isConnected){
+            LatLng place = new LatLng(location.getLatitude(), location.getLongitude());
+            marker = map.addMarker(new MarkerOptions()
+                    .draggable(true)
+                    .position(place));
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 14));
+        }else{
+            latitudeText.setText(String.valueOf(location.getLatitude()));
+            longitudeText.setText(String.valueOf(location.getLongitude()));
+            accuracyText.setText(String.valueOf(location.getAccuracy()) + 'm');
+        }
     }
 
     @Override
@@ -139,14 +162,17 @@ public class AppUserMarkerMapActivity extends AppCompatActivity implements Googl
 
     @Click
     protected void locationBtn(){
+        AppUserMarker aum = new AppUserMarker();
+        aum.setMarkerId(appMarker.getId());
         if(marker != null){
-            AppUserMarker aum = new AppUserMarker();
-            aum.setMarkerId(appMarker.getId());
             LatLng pos = marker.getPosition();
             aum.setLat(pos.latitude);
             aum.setLng(pos.longitude);
-            Bundle args = aumDao.toArgs(aum);
-            app.startAppUserMarkerActivity(this, args);
+        }else if(marker == null && location != null){
+            aum.setLat(location.getLatitude());
+            aum.setLng(location.getLongitude());
         }
+        Bundle args = aumDao.toArgs(aum);
+        app.startAppUserMarkerActivity(this, args);
     }
 }
